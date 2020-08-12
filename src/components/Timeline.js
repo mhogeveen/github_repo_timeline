8cb17036c8472411e32a2f5368a90a04e41c42ba
Maarten Hogeveen <maarten@192.168.2.14>
2020-08-12 15:18:06 +0200

    First pass userinfo card

--- src/components/Timeline.js
+++ src/components/Timeline.js
@@ -1,19 +1,23 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { connect } from 'react-redux'
 
 import Back from './Back'
+import UserInfo from './UserInfo'
 
-const Timeline = () => {
+import { getUserInfo, updateUsername } from '../actions'
+
+const Timeline = ({ userInfo, getUserInfo, updateUsername }) => {
+   useEffect(() => {
+      updateUsername('mhogeveen')
+      getUserInfo()
+   }, [updateUsername, getUserInfo])
+
    return (
       <>
          <Back />
          <div className='timeline-container'>
             <div className='timeline-item ui raised segment'>
-               Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus lacinia suscipit
-               vestibulum. Suspendisse ultricies enim neque, sit amet pulvinar felis dignissim eu.
-               Cras nec sapien eu nisi mollis scelerisque. Aenean ultricies fringilla varius. Nullam
-               aliquam, lacus sit amet posuere bibendum, odio nulla mollis metus, in fermentum lacus
-               nunc id urna.
+               <UserInfo userInfo={userInfo} />
             </div>
             <div className='timeline-item ui raised segment'>
                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus lacinia suscipit
@@ -45,4 +49,4 @@ const mapStateToProps = (state) => {
    return { userInfo: state.userInfo, userRepos: state.userRepos }
 }
 
-export default connect(mapStateToProps)(Timeline)
+export default connect(mapStateToProps, { getUserInfo, updateUsername })(Timeline)
---
+++ src/components/UserInfo.js
@@ -0,0 +1,28 @@
+import React from 'react'
+
+const UserInfo = ({ userInfo }) => {
+   const returnDateString = () => {
+      const dateParse = new Date(userInfo.created_at)
+      const dateString = dateParse.toLocaleDateString()
+      return dateString
+   }
+
+   return (
+      <div className='userinfo-container'>
+         <div className='ui tiny circular image'>
+            <img src={userInfo.avatar_url} alt='User avatar' />
+         </div>
+         <div className='userinfo-content'>
+            <div className='content-name'>{`User: ${userInfo.login}`}</div>
+            <div className='content-info'>{`Name: ${userInfo.name}`}</div>
+            <div className='content-info'>{`Created on: ${returnDateString()}`}</div>
+            <div className='content-info'>{`Public repos: ${userInfo.public_repos}`}</div>
+            <a href={userInfo.html_url} className='content-link'>
+               <i className='teal external square alternate link icon'></i>
+            </a>
+         </div>
+      </div>
+   )
+}
+
+export default UserInfo
