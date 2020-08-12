d2f5bcc6a9f4646c0567f2d3842607e04fe21725
Maarten Hogeveen <maarten@192.168.2.14>
2020-08-12 16:53:29 +0200

    Add animation on timeline items

--- src/components/Timeline.js
+++ src/components/Timeline.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import { connect } from 'react-redux'
 
 import Back from './Back'
@@ -7,42 +7,118 @@ import UserInfo from './UserInfo'
 import { getUserInfo, updateUsername } from '../actions'
 
 const Timeline = ({ userInfo, getUserInfo, updateUsername }) => {
+   const [loading, setLoading] = useState(true)
+
    useEffect(() => {
       updateUsername('mhogeveen')
       getUserInfo()
+
+      setTimeout(() => {
+         setLoading(false)
+
+         const observer = new IntersectionObserver(
+            (entries) => {
+               entries.forEach((entry) => {
+                  if (entry.isIntersecting === true) {
+                     entry.target.classList.add('show-on-scroll')
+                  }
+               })
+            },
+            [1]
+         )
+
+         const targets = document.querySelectorAll('.timeline-item')
+         targets.forEach((target) => {
+            observer.observe(target)
+         })
+      }, 2000)
    }, [updateUsername, getUserInfo])
 
-   return (
-      <>
-         <Back />
-         <div className='timeline-container'>
-            <div className='timeline-item ui raised segment'>
-               <UserInfo userInfo={userInfo} />
+   const renderContent = () => {
+      if (loading) {
+         return (
+            <div className='ui active inverted dimmer'>
+               <div className='ui loader'></div>
             </div>
-            <div className='timeline-item ui raised segment'>
-               Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus lacinia suscipit
-               vestibulum. Suspendisse ultricies enim neque, sit amet pulvinar felis dignissim eu.
-               Cras nec sapien eu nisi mollis scelerisque. Aenean ultricies fringilla varius. Nullam
-               aliquam, lacus sit amet posuere bibendum, odio nulla mollis metus, in fermentum lacus
-               nunc id urna.
-            </div>
-            <div className='timeline-item ui raised segment'>
-               Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus lacinia suscipit
-               vestibulum. Suspendisse ultricies enim neque, sit amet pulvinar felis dignissim eu.
-               Cras nec sapien eu nisi mollis scelerisque. Aenean ultricies fringilla varius. Nullam
-               aliquam, lacus sit amet posuere bibendum, odio nulla mollis metus, in fermentum lacus
-               nunc id urna.
-            </div>
-            <div className='timeline-item ui raised segment'>
-               Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus lacinia suscipit
-               vestibulum. Suspendisse ultricies enim neque, sit amet pulvinar felis dignissim eu.
-               Cras nec sapien eu nisi mollis scelerisque. Aenean ultricies fringilla varius. Nullam
-               aliquam, lacus sit amet posuere bibendum, odio nulla mollis metus, in fermentum lacus
-               nunc id urna.
-            </div>
-         </div>
-      </>
-   )
+         )
+      } else {
+         return (
+            <>
+               <Back />
+               <div className='timeline-container'>
+                  <div className='timeline-item ui raised segment'>
+                     <UserInfo userInfo={userInfo} />
+                  </div>
+                  <div className='timeline-item ui raised segment'>
+                     Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus lacinia
+                     suscipit vestibulum. Suspendisse ultricies enim neque, sit amet pulvinar felis
+                     dignissim eu. Cras nec sapien eu nisi mollis scelerisque. Aenean ultricies
+                     fringilla varius. Nullam aliquam, lacus sit amet posuere bibendum, odio nulla
+                     mollis metus, in fermentum lacus nunc id urna.
+                  </div>
+                  <div className='timeline-item ui raised segment'>
+                     Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus lacinia
+                     suscipit vestibulum. Suspendisse ultricies enim neque, sit amet pulvinar felis
+                     dignissim eu. Cras nec sapien eu nisi mollis scelerisque. Aenean ultricies
+                     fringilla varius. Nullam aliquam, lacus sit amet posuere bibendum, odio nulla
+                     mollis metus, in fermentum lacus nunc id urna.
+                  </div>
+                  <div className='timeline-item ui raised segment'>
+                     Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus lacinia
+                     suscipit vestibulum. Suspendisse ultricies enim neque, sit amet pulvinar felis
+                     dignissim eu. Cras nec sapien eu nisi mollis scelerisque. Aenean ultricies
+                     fringilla varius. Nullam aliquam, lacus sit amet posuere bibendum, odio nulla
+                     mollis metus, in fermentum lacus nunc id urna.
+                  </div>
+                  <div className='timeline-item ui raised segment'>
+                     Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus lacinia
+                     suscipit vestibulum. Suspendisse ultricies enim neque, sit amet pulvinar felis
+                     dignissim eu. Cras nec sapien eu nisi mollis scelerisque. Aenean ultricies
+                     fringilla varius. Nullam aliquam, lacus sit amet posuere bibendum, odio nulla
+                     mollis metus, in fermentum lacus nunc id urna.
+                  </div>
+                  <div className='timeline-item ui raised segment'>
+                     Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus lacinia
+                     suscipit vestibulum. Suspendisse ultricies enim neque, sit amet pulvinar felis
+                     dignissim eu. Cras nec sapien eu nisi mollis scelerisque. Aenean ultricies
+                     fringilla varius. Nullam aliquam, lacus sit amet posuere bibendum, odio nulla
+                     mollis metus, in fermentum lacus nunc id urna.
+                  </div>
+                  <div className='timeline-item ui raised segment'>
+                     Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus lacinia
+                     suscipit vestibulum. Suspendisse ultricies enim neque, sit amet pulvinar felis
+                     dignissim eu. Cras nec sapien eu nisi mollis scelerisque. Aenean ultricies
+                     fringilla varius. Nullam aliquam, lacus sit amet posuere bibendum, odio nulla
+                     mollis metus, in fermentum lacus nunc id urna.
+                  </div>
+                  <div className='timeline-item ui raised segment'>
+                     Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus lacinia
+                     suscipit vestibulum. Suspendisse ultricies enim neque, sit amet pulvinar felis
+                     dignissim eu. Cras nec sapien eu nisi mollis scelerisque. Aenean ultricies
+                     fringilla varius. Nullam aliquam, lacus sit amet posuere bibendum, odio nulla
+                     mollis metus, in fermentum lacus nunc id urna.
+                  </div>
+                  <div className='timeline-item ui raised segment'>
+                     Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus lacinia
+                     suscipit vestibulum. Suspendisse ultricies enim neque, sit amet pulvinar felis
+                     dignissim eu. Cras nec sapien eu nisi mollis scelerisque. Aenean ultricies
+                     fringilla varius. Nullam aliquam, lacus sit amet posuere bibendum, odio nulla
+                     mollis metus, in fermentum lacus nunc id urna.
+                  </div>
+                  <div className='timeline-item ui raised segment'>
+                     Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus lacinia
+                     suscipit vestibulum. Suspendisse ultricies enim neque, sit amet pulvinar felis
+                     dignissim eu. Cras nec sapien eu nisi mollis scelerisque. Aenean ultricies
+                     fringilla varius. Nullam aliquam, lacus sit amet posuere bibendum, odio nulla
+                     mollis metus, in fermentum lacus nunc id urna.
+                  </div>
+               </div>
+            </>
+         )
+      }
+   }
+
+   return renderContent()
 }
 
 const mapStateToProps = (state) => {
